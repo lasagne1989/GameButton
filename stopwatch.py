20a210a4076ae051b432e0a604633b86e5598f68
lasagne1989 <101409249+lasagne1989@users.noreply.github.com>
2022-04-06 15:52:30 +0100

    working but 1st name switch

--- stopwatch.py
+++ stopwatch.py
@@ -55,7 +55,6 @@ class Timer:
                 pick = firstplayer.playerPicker(self.people, self.playerNum)
                 self.playerNum = pick.nextPlayer()
                 self.player = self.people[self.playerNum]
-                self.display2.destroy()
                 self.timeit = not self.timeit
         else:
             self.display3.destroy()
