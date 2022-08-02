199eadeaa8bc90a641716c2c84c944c884a16e18
lasagne1989 <101409249+lasagne1989@users.noreply.github.com>
2022-08-02 14:13:24 +0100

    add websockets - working

--- websockSVR.py
+++ websockSVR.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
+
 import asyncio
 import websockets
 
-async def handler(websocket):
-    while True:
-        message = await websocket.recv()
-        print(message)
+async def hello(websocket):
+    name = await websocket.recv()
+    print(f"<<< {name}")
 
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f">>> {greeting}")
 
 async def main():
-    async with websockets.serve(handler, "", 8001):
-        await asyncio.Future() #run forever
+    async with websockets.serve(hello, "192.168.0.3", 8765):
+        await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
     asyncio.run(main())
