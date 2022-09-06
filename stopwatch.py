86f602438f3b08d949bcf534afd8c20d44e3af55
lasagne1989 <101409249+lasagne1989@users.noreply.github.com>
2022-09-06 16:49:33 +0100

    Update stopwatch.py

--- stopwatch.py
+++ stopwatch.py
@@ -4,6 +4,7 @@
 #py files
 import press
 #libraries
+import json
 import websockets
 import asyncio
 import random
@@ -14,8 +15,7 @@ from threading import *
 import RPi.GPIO as GPIO
 
 root = Tk()
-#global players
-#subprocess.call("python3 ./websockSVR.py", shell=True)
+
 
 class Timer:
     def __init__(self, master):
@@ -29,41 +29,31 @@ class Timer:
         self.display2 = Label(master, fg='white', bg='grey9', font=("Ariel", 35))
         self.display2.place(relx=.5, rely=.5, anchor=N)
         self.display1['text'] = 'Wait for Phone Connection'
-        # Set up main button press
-        #reg_event = press.Button(GPIO.IN, GPIO.PUD_DOWN, GPIO.FALLING, self.start)
-        #reg_event.setup()
-        #reg_event.event()
         # Press count variable
         self.time_limit = 5
         self.press_count = 0
         # Start Websocket
         t = Thread(target=self.sockSVR).start()  
-        #websocks = "/home/pi/GameButton/websockSVR.py"
-        #subprocess.run("python3 ./websockSVR.py", shell=True)
         # Pick first player and set up cycle
-        #people = [players]
-        #player_count = len(people)
-        #player_num = random.randint(0, player_count - 1)
         self.players = []
         self.player_cycle = []
-        #for i in range(player_count):
-        #    player_cycle.append(people[player_num % player_count])
-        #    player_num += 1
-        
     
     def connection(self):
         self.display1['text'] = 'Connected and Ready, Press the Button'
+        # Set up main button press
         reg_event = press.Button(GPIO.IN, GPIO.PUD_DOWN, GPIO.FALLING, self.start)
         reg_event.setup()
         reg_event.event()
-        people = [self.players]
-        player_count = len(people)
+        # Pick first player and set up cycle
+        #people = [self.players]
+        player_count = len(self.players)
         player_num = random.randint(0, player_count - 1)
         self.player_cycle = []
         for i in range(player_count):
-            self.player_cycle.append(people[player_num % player_count])
+            self.player_cycle.append(self.players[player_num % player_count])
             player_num += 1
         self.next_players = itertools.cycle(self.player_cycle)    
+    
     def start(self, channel):
         # Show randomised First player on first press
         if self.press_count == 0:
@@ -107,20 +97,24 @@ class Timer:
     
     def sockSVR(self):
         async def handler(websocket):
-            self.players = await websocket.recv()
-            print(f"{self.players}")
+            msg = await websocket.recv()
+            print(msg)
+            dict = json.loads(msg)
+            print(dict)
+            print(dict['time_limit'])
+            print(dict['players'])
+            self.time_limit = dict['time_limit']
+            self.players = dict['players']
+            #print(f"{self.players}")
             start = f"start"
             await websocket.send(start)
             self.connection()
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
     
-        start_server= websockets.serve(handler, "localhost", 8765)
+        start_server= websockets.serve(handler, "192.168.0.26", 8765)
         loop.run_until_complete(start_server)
         loop.run_forever()
-
-
-      
 
 if __name__=="__main__":
     app = Timer(root)
