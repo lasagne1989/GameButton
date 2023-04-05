805b8cd959ce573ed84d7e1cfc50e997ff76cc04
lasagne1989 <101409249+lasagne1989@users.noreply.github.com>
2023-04-05 11:23:53 +0100

    Websocket should now return msg

--- websocket.py
+++ websocket.py
@@ -5,6 +5,8 @@ from _socket import gethostname, gethostbyname
 from websockets import serve
 import asyncio
 
+msg = str
+
 
 def sockSVR():
     print("running...")
@@ -14,15 +16,19 @@ def sockSVR():
     print(ip_addr)
 
     async def handler(websocket):
+        global msg
         msg = await websocket.recv()
         print(msg)
-        return msg
+        start = f"start"
+        await websocket.send(start)
 
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     start_server = serve(handler, "%s" % ip_addr, 8765)
     loop.run_until_complete(start_server)
     loop.run_forever()
+    return msg
+
 
 if __name__ == "__main__":
     sockSVR()
