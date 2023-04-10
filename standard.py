5978b3ccf541567a416f90cda87de5cb2729743b
lasagne1989 <101409249+lasagne1989@users.noreply.github.com>
2023-04-10 19:55:07 +0100

    added button press

--- standard.py
+++ standard.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from time import sleep
+from press import Button
+import RPi.GPIO as GPIO
 from random import randint
 from itertools import cycle
 from tkinter import *
@@ -10,17 +12,18 @@ root = Tk()
 class Standard:
     def __init__(self, time_limit, players, master):
         self.master = master
-
+        #set up screen
         root.config(cursor="none")
         #root.geometry("320x240")
         root.attributes('-fullscreen', True)
         root['bg'] = 'grey9'
         root.attributes("-topmost", True)
-
+        #set up labels
         self.playing = Label(master, fg='white', bg='grey9', font=("Ariel", 32), wraplength=318)
         self.playing.place(relx=.5, rely=.5, anchor="s")
         self.timer = Label(master, fg='white', bg='grey9', font=("Ariel", 35), wraplength=318)
         self.timer.place(relx=.5, rely=.5, anchor="n")
+        #initialise variables
         self.time_limit = time_limit
         self.time_text = DoubleVar()
         self.player_text = DoubleVar()
@@ -28,14 +31,24 @@ class Standard:
         self.next_player = []
         self.time_limit = time_limit
         self.players = players
-        root.update()
+        #update screen
+        #root.update()
+        #select first player
         self.first_player()
-        self.countdown()
+        self.player = next(self.next_player)
+        self.player_text.set(self.player)
+        self.playing['textvariable'] = self.player_text
+        self.playing['text'] = f"{self.player}, You Go First!"
+        self.timer['text'] = "You Go First"
+        reg_event = Button(GPIO.IN, GPIO.PUD_DOWN, GPIO.FALLING, self.countdown)
+        reg_event.setup()
+        reg_event.event()
+        #self.countdown()
 
     def countdown(self):
         time_left = self.time_limit
         self.time_text.set(time_left)
-        self.player = next(self.next_player)
+        self.player = self.next_player[1]
         self.player_text.set(self.player)
         self.playing['textvariable'] = self.player_text
         print(self.player)
@@ -54,9 +67,8 @@ class Standard:
         self.restart()
 
     def restart(self):
-        # add await button press
-        sleep(1)
-        self.countdown()
+        wait_event = Button(GPIO.IN, GPIO.PUD_DOWN, GPIO.FALLING, self.countdown())
+        wait_event.wait()
 
     def first_player(self):
         player_cycle = []
